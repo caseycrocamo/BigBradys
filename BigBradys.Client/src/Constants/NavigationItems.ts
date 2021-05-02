d050ea00836eb3bcb911ab931e00bf89cbb5a3f5
Casey Crocamo <casey.crocamo@alliant.com>
2021-05-02 16:43:03 -0700

    Add about page add routing

---
+++ BigBradys.Client/src/Constants/NavigationItems.ts
@@ -0,0 +1,18 @@
+﻿import {FunctionComponent} from "react";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+
+class navigationItem{
+    constructor(name: string, route: string, component: FunctionComponent) {
+        this.name = name;   
+        this.route = route;
+        this.component = component;
+    }
+    name: string;
+    route: string;
+    component: FunctionComponent;
+}
+export const navigationItems: navigationItem[] = [
+    new navigationItem("Home", "/", Home),
+    new navigationItem("About", "/about", About),
+]
