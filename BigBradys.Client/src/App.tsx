f2db3c511960585681e654dcd9c129feeb772402
Casey Crocamo <casey.crocamo@gmail.com>
2021-07-10 22:08:18 -0700

    remove search bar

--- BigBradys.Client/src/App.tsx
+++ BigBradys.Client/src/App.tsx
@@ -6,7 +6,6 @@ import {
 } from "react-router-dom";
 import React from 'react'
 import { Disclosure } from '@headlessui/react'
-import { SearchIcon } from '@heroicons/react/solid'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import {navigationItems} from "./Constants/NavigationItems";
 
@@ -35,25 +34,6 @@ function App() {
                                 {navigationItems.map((item, itemIdx) =>
                                     <NavLink className="text-white hover:bg-indigo-500 hover:bg-opacity-75 rounded-md py-2 px-3 text-sm font-medium" activeClassName="bg-indigo-700 text-white rounded-md py-2 px-3 text-sm font-medium" key={itemIdx} to={item.route} exact>{item.name}</NavLink>
                                 )}
-                              </div>
-                            </div>
-                          </div>
-                          <div className="flex-1 px-2 flex justify-center lg:ml-6 lg:justify-end">
-                            <div className="max-w-lg w-full lg:max-w-xs">
-                              <label htmlFor="search" className="sr-only">
-                                Search
-                              </label>
-                              <div className="relative text-gray-400 focus-within:text-gray-600">
-                                <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
-                                  <SearchIcon className="h-5 w-5" aria-hidden="true" />
-                                </div>
-                                <input
-                                    id="search"
-                                    className="block w-full bg-white py-2 pl-10 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white focus:border-white sm:text-sm"
-                                    placeholder="Search"
-                                    type="search"
-                                    name="search"
-                                />
                               </div>
                             </div>
                           </div>
