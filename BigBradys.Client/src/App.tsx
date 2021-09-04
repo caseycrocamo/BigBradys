8a3caba2e8f0e45591b562ba116aa645d37e0ab3
Casey Crocamo <casey.crocamo@gmail.com>
2021-09-04 15:54:40 -0400

    finished styling the navbar

--- BigBradys.Client/src/App.tsx
+++ BigBradys.Client/src/App.tsx
@@ -14,17 +14,17 @@ const profile = ['Your Profile', 'Settings', 'Sign out']
 function App() {
   return (
       <Router>
-          <div className="min-h-screen bg-gray-100">
-            <div className="navbar pb-32">
+          <div className="min-h-screen bg-purple bg-opacity-70">
+            <div className="navbar pb-24 shadow-md">
               <Disclosure as="nav" className="navbar">
                 {({ open }) => (
                     <>
                       <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
-                        <div className="relative h-16 flex items-center justify-between">
+                        <div className="relative h-24 flex items-center justify-between">
                           <div className="px-2 flex items-center lg:px-0">
                             <div className="flex-shrink-0">
                               <img
-                                  className="block h-16"
+                                  className="block h-24"
                                   src="/logo.png"
                                   alt="Workflow"
                               />
@@ -39,7 +39,7 @@ function App() {
                           </div>
                           <div className="flex md:hidden">
                             {/* Mobile menu button */}
-                            <Disclosure.Button className="bg-purple p-2 rounded-md inline-flex items-center justify-center text-purple-light hover:text-white hover:bg-purple hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-purple focus:ring-purple=lightest">
+                            <Disclosure.Button className="bg-purple p-2 rounded-md inline-flex items-center justify-center text-white hover:text-white hover:bg-purple hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-purple focus:ring-purple=lightest">
                               <span className="sr-only">Open main menu</span>
                               {open ? (
                                   <XIcon className="block h-6 w-6" aria-hidden="true" />
